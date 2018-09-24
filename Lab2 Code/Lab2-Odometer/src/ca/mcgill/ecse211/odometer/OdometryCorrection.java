5309dd965cb011ad59283a6d6e45018577552035
Hyun Su An <hyun.an@mail.mcgill.ca>
2018-09-24 17:41:58 -0400

    Lab 2 update

--- Lab2 Code/Lab2-Odometer/src/ca/mcgill/ecse211/odometer/OdometryCorrection.java
+++ Lab2 Code/Lab2-Odometer/src/ca/mcgill/ecse211/odometer/OdometryCorrection.java
@@ -12,7 +12,7 @@ import lejos.robotics.SampleProvider;
 
 public class OdometryCorrection implements Runnable {
 	private static final long CORRECTION_PERIOD = 10;
-	private OdometerData odoData;
+	private Odometer odoData;
 	private static final Port csPort = LocalEV3.get().getPort("S1");
 	private float[] csData;
 	private SampleProvider ColorID;
@@ -37,7 +37,7 @@ public class OdometryCorrection implements Runnable {
 		// 3. Create a sample provider instance for the above and initialize operating mode
 		// 4. Create a buffer for the sensor data
 
-		this.odoData = OdometerData.getOdometerData();
+		this.odoData = Odometer.getOdometer();
 		
 		//2.Sensor instance
 		SensorModes ColorSensor = new EV3ColorSensor(csPort);
@@ -58,37 +58,43 @@ public class OdometryCorrection implements Runnable {
 	// run method (required for Thread)
 	public void run() {
 		long correctionStart, correctionEnd;
+		double offset = 3.5;
 		while (true) {
 			correctionStart = System.currentTimeMillis();
 			ColorID.fetchSample(csData, 0);
 			float intensity = csData[0]; //last value sent by sensor
 			// encounters a black line
 			// all numbers within if-condition correspond to range of angle (in degrees) on square corners
-			if(intensity<0.2) {
+			if(intensity<0.21) {
 				Sound.playTone(3, 100);
-				if(odoData.getXYT()[2] >= 0.00 && odoData.getXYT()[2] <= 20){ // case 1: keep increasing y-position until wheels turn 90 degrees
-					odoData.setY(county*size); // robot is horizontally in line with origin
+				double angle =odoData.getXYT()[2];
+				if(angle >= 0.00 && angle <= 40){ // case 1: keep increasing y-position until wheels turn 90 degrees
+					odoData.setY(county*size -offset); // robot is horizontally in line with origin
 					county++;
-					LCD.drawString("case1", 0, 1);
+					LCD.drawString("case 1 " + county +" " + intensity, 0, 5);
 				}
-				else if(odoData.getXYT()[2] >= 78 && odoData.getXYT()[2] <= 105){ // case 2: keep increasing x-position until 90 degree turn
-					odoData.setX(countx*size);
+				else if(angle >= 78 && angle <= 105){ // case 2: keep increasing x-position until 90 degree turn
+					odoData.setX(countx*size -offset);
 					countx++;
-					LCD.drawString("case2", 0, 2);
+					LCD.drawString("case 2 "+ countx, 0, 5);
+
 				}// robot is vertically in line with origin
-				else if(odoData.getXYT()[2] >= 165 && odoData.getXYT()[2] <= 193){ // case 3: keep decreasing y-position until 90 degree turn
+				else if(angle >= 165 && angle <= 193){ // case 3: keep decreasing y-position until 90 degree turn
 					county--;
-					odoData.setY(county*size);
-					LCD.drawString("case3", 0, 3);
+					odoData.setY(county*size -offset);
+					LCD.drawString("case 3 " + county, 0, 5);
+
 				}	
-				else if(odoData.getXYT()[2] >= 260 && odoData.getXYT()[2] <= 284){ // case 4: keep decreasing x-position until 90 degree turn
+				else if(angle >= 260 && angle <= 284){ // case 4: keep decreasing x-position until 90 degree turn
 					countx--;
-					odoData.setX(countx*size);
-					LCD.drawString("case4", 0, 4);
+					odoData.setX(countx*size - offset);
+					LCD.drawString("case 4 " + countx, 0, 5);
+
 				}	 
 			}
 		
-
+			//last_intensity = intensity;
+			
 			// this ensure the odometry correction occurs only once every period
 			correctionEnd = System.currentTimeMillis();
 			if (correctionEnd - correctionStart < CORRECTION_PERIOD) {
